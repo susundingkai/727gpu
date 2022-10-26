4f5822f9ea7cf33d4f554a85eb49440dc7b2d7e7
susundingkai <326728639@qq.com>
2022-10-26 21:18:38 +0800

    add refresh

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,20 @@
 // index.js
 // 获取应用实例
 const app = getApp()
-
+var socket = {};
 Page({
   data: {
     machineIPs: [],
     gpuInfo: {}
   },
-  // 事件处理函数
-  onLoad(options) {
+  connect_server() {
     var _this = this;
     //创建websocket
     //正式地址使用wss
-    var socket = wx.connectSocket({
+    wx.closeSocket({complete(res){
+      console.log('res:', res)
+    }})
+    socket = wx.connectSocket({
       url: 'wss://pris.ssdk.icu/portal',
       success: res => {
         console.info('创建连接成功');
@@ -46,11 +48,30 @@ Page({
         machineIPs: _this.data.machineIPs,
         gpuInfo: _info
       })
-      // var list = _this.data.result;
-      // list = list.concat([data]);
-      // _this.setData({
-      //   result: list
-      // });
     });
+  },
+  // 事件处理函数
+  onLoad(options) {
+    this.connect_server()
+  },
+  onRefresh() {
+    //在当前页面显示导航条加载动画
+    wx.showNavigationBarLoading();
+    //显示 loading 提示框。需主动调用 wx.hideLoading 才能关闭提示框
+    wx.showLoading({
+      title: '刷新中...',
+    })
+    console.log("refresh")
+    this.connect_server();
+    //隐藏loading 提示框
+    wx.hideLoading();
+    //隐藏导航条加载动画
+    wx.hideNavigationBarLoading();
+    //停止下拉刷新
+    wx.stopPullDownRefresh();
+  },
+  onPullDownRefresh: function () {
+    //调用刷新时将执行的方法
+    this.onRefresh();
   }
 })
